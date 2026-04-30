d162b1f820b5d53e050851c0d88ee4c876ece443
20140964-art <20140964@s.ubaguio.edu>
2026-04-30 10:54:46 +0800

    LOGIN Change username input to email in login form

--- ams/index.php
+++ ams/index.php
@@ -91,7 +91,7 @@
 
         <form action="login.php" method="POST">
 
-            <input type="text" name="username" placeholder="Username" required>
+            <input type="text" name="Email" placeholder="Email" required>
 
             <input type="password" name="password" placeholder="Password" required>
 
