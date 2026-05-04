82692df598d8b3b2bf97d8e047565e505f4fc468
Keroleum <kerosensible@gmail.com>
2026-05-05 03:07:23 +0800

    removed database integrity checkers, im still tired boss

--- verify_schema.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'ams/config/config.php';
-
-$cols = $pdo->query('DESCRIBE students')->fetchAll(PDO::FETCH_ASSOC);
-foreach ($cols as $col) {
-    echo $col['Field'] . ": " . $col['Type'] . "\n";
-}
-?>
