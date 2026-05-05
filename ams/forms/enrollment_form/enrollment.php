35a0b0217d32a9d51d1ef2f15599bcc9fecad2ba
chroma-ken <20240279@s.ubaguio.edu>
2026-05-05 11:53:38 +0800

    Edited the Back to Home button location

--- ams/forms/enrollment_form/enrollment.php
+++ ams/forms/enrollment_form/enrollment.php
@@ -439,7 +439,7 @@ footer strong { color: rgba(255,255,255,.8); }
             <img src="../../style/logo.png" alt="Logo">
             <span>Gibraltar Elementary School</span>
         </div>
-        <a href="../../login/index.php">← Back to Home</a>
+        <a href="../../login/login.php/">← Back to Home</a>
     </div>
 
     <!-- PROGRESS STEPPER -->
